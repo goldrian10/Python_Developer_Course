6cc58945455751b67d17be9033f0bb5c9595e6bc
goldrian10 <estebanaga10@gmail.com>
2021-06-13 22:01:56 -0600

    Update unit test file

--- Unittest/test.py
+++ Unittest/test.py
@@ -1,17 +1,33 @@
 import unittest
 import main
 
-
 class TestMain(unittest.TestCase):
+    def setUp(self):
+        print('About to run a function')
     def test_do_stuff(self):
-        num = 10
-        result = main.do_stuff(num)
-        self.assertEqual(result, 15)
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 20)
 
-    def test_do_stuff(self):
-        num = 'sdasdasd'
-        result = main.do_stuff(num)
-        self.assertEqual(result, ValueError)
+    def test_do_stuff2(self):
+        test_param = "dasdasda"
+        result = main.do_stuff(test_param)
+        self.assertTrue(isinstance(result, ValueError))
+        # self.assertIsInstance(result, ValueError)
 
+    def test_do_stuff3(self):
+        test_param = None
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 'Please enter a number')
 
-unittest.main()
+    def test_do_stuff4(self):
+        test_param = ''
+        result = main.do_stuff(test_param)
+        self.assertIsInstance(result, ValueError)
+
+    def tearDown(self):
+        print('clenaning up')
+
+if __name__ == "__main__":
+
+    unittest.main()
